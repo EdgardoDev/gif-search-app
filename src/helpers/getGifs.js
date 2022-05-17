cb901bf8dbf4e09dca82762aa3d2e61e03dff8dd
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2022-05-17 08:53:47 +0200

    Completed more tests on the GifTemplateItem component

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,5 @@
 export const getGifs = async (appCategory) => {
 
-
     const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(appCategory)}&limit=10&api_key=YOUR_API_KEY`
     const res = await fetch(url);
     const { data } = await res.json();
--- src/tests/components/GifTemplateItem.test.js
+++ src/tests/components/GifTemplateItem.test.js
@@ -5,10 +5,30 @@ import { GifTemplateItem } from '../../components/GifTemplateItem';
 describe('Tests for <GifTemplateItem />', () => {
 
   const title = 'title';
-  const url = 'https://localhost:8080/';
+  const url = 'https://localhost/image.jpg';
+  const wrapper = shallow(<GifTemplateItem title={title} url={url} />);
 
   test('It should show the component without any problems', () => {
-    const wrapper = shallow(<GifTemplateItem title={title} url={url} />);
     expect(wrapper).toMatchSnapshot();
   })
+
+  test('It has to have a parragraph with the title', () => {
+    const parragraph = wrapper.find('p');
+    expect(parragraph.text().trim()).toBe(title);
+  })
+
+  test('It should have an image that matches the url and alt of the props', () => {
+    const image = wrapper.find('img');
+    expect(image.prop('src')).toBe(url);
+    expect(image.prop('alt')).toBe(title);
+
+  })
+
+  test('It should have the class animate__bounce', () => {
+    const div = wrapper.find('div');
+    const className = div.prop('className');
+    expect(className.includes('animate__bounce')).toBe(true);
+  })
+
+
 });
