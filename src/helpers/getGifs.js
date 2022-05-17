0ea25612759195d3a7372fdb007d04641533747c
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2022-05-17 12:01:23 +0200

    Added new tests for the getGifs Fetch helper

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async (appCategory) => {
 
-    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(appCategory)}&limit=10&api_key=gV2g5RbnFCXlwmiHouziBvlciZtnWpOj`
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(appCategory)}&limit=10&api_key=YOUR_API_KEY`
     const res = await fetch(url);
     const { data } = await res.json();
 
