deda9963ca001eb68c3f4d18bc286f0def500bab
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2022-05-22 09:12:12 +0200

    Added a new test to simulate form submition

--- src/tests/components/AddNewCategory.test.js
+++ src/tests/components/AddNewCategory.test.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import '@testing-library/jest-dom';
+
 import { shallow } from 'enzyme';
 import { AddNewCategory } from '../../components/AddNewCategory';
 
 describe('Tests for the component <AddNewCategory />', () => {
 
-  const setAppCategories = () => { };
-  const wrapper = shallow(<AddNewCategory setAppCategories={setAppCategories} />)
+  const setAppCategories = jest.fn();
+  let wrapper;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    wrapper = shallow(<AddNewCategory setAppCategories={setAppCategories} />)
+  })
 
   test('It should render without any problems.', () => {
     expect(wrapper).toMatchSnapshot();
@@ -17,6 +24,11 @@ describe('Tests for the component <AddNewCategory />', () => {
     input.simulate('change', { target: { value } });
 
     expect(wrapper.find('p').text().trim()).toBe(value);
+  })
+
+  test('It should NOT post information on submit', () => {
+    wrapper.find('form').simulate('submit', { preventDefault() { } });
+    expect(setAppCategories).not.toHaveBeenCalled();
   })
 
 })
